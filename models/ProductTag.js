edd48459f3fe86c85bfdb13b16884931a4611f94
j92pruitt <j92pruitt@gmail.com>
2021-08-05 19:06:51 -0700

    Added product tag

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -14,32 +14,18 @@ ProductTag.init(
       autoIncrement: true
     },
 
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-
-    price: {
-      type: DataTypes.DECIMAL,
-      allowNull: false,
-      validate: {
-        isDecimal: true
-      }
-    },
-
-    stock: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 10,
-      validate: {
-        isNumeric: true
-      }
-    },
-
-    category_id: {
+    product_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
+        model: 'product',
+        key: 'id'
+      }
+    },
+
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
         key: 'id'
       }
     }
