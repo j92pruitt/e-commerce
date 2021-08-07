24b506a0a14f6765364afa170a298b2e4222fb1b
j92pruitt <j92pruitt@gmail.com>
2021-08-07 14:42:02 -0700

    Added route for creating new tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -22,10 +22,34 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+try {
+  
+  const tagData = await Tag.findByPk( req.params.id, {
+    include: [{model: Product}]
+  });
+
+  if (!tagData) {
+    res.status(404).json({ message: 'No user with this id!' })
+    return;
+  }
+  res.status(200).json(tagData);
+
+} catch (err) {
+  res.status(500).json(err)
+}
+
 });
 
 router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.put('/:id', async (req, res) => {
