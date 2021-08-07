b16ad258a27d294385028c2f5d41de73e7d62edf
j92pruitt <j92pruitt@gmail.com>
2021-08-06 19:22:56 -0700

    Added pot route for creating new categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,11 +31,17 @@ router.get('/:id', async (req, res) => {
   } catch {
     res.status(500).json(err);
   }
-  
+
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch {
+    res.status(500).json(categoryData);
+  }
 });
 
 router.put('/:id', (req, res) => {
